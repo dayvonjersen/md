d1971247a513a59df5cfeda2ef46568216fb539d
tso <tso@teknik.io>
2018-09-23 20:39:37 -0400

    make css less shit by stealing it from someone who knew what they were doing

--- main.go
+++ main.go
@@ -87,95 +87,178 @@ const boilerplateHTML = `<!DOCTYPE html>
 
 const boilerplateCSS = `
         <style>
-body {
-    max-width: 800px;
-    margin: auto;
-    font-family: "Noto Sans", sans-serif;
-    background: #fff;
-    color: #272727;
-}
-::selection {
-    background: #272727;
-    color: #fff;
-}
 :root {
     font-size: 14px;
-    line-height: 1.6875;
+    line-height: 1.5;
 }
-h1,h2,h3,h4,h5,h6 {
-    margin: 0;
+
+body {
+    font-family: "Noto Sans", sans-serif;
+    background: #fefefe;
+    color: #444;
+    padding: 1rem;
+    max-width: 42rem;
+    margin: auto;
 }
-h1 {
-    font-size: 3rem;
-    line-height: 1.2638;
-    border-bottom: 1px ridge;
+
+::selection, a::selection {
+    background: rgba(255,255,0,0.3);
+    color: #000;
 }
-h2 {
-    font-size: 2.5rem;
-    line-height: 1.3697;
+
+a::selection {
+    color: #0645ad;
 }
-h3 {
-    font-size: 2rem;
-    line-height: 1.4757;
-}
-h4 {
-    font-size: 1.5rem;
-    line-height: 1.5816;
-}
-h5 {
-    font-size: 1.25rem;
-    line-height: 1.6875;
-}
-h6 {
-    font-size: 1rem;
-    line-height: 1.6875;
-}
-hr {
-    border: 1px ridge;
-}
-blockquote {
-    background: #eee;
-    margin: 0;
-    padding: 1px 1em;
-    border-left: 5px solid #ccc;
-}
-pre, :not(pre) > code {
-    background: #272727;
-    color: #fff;
-    padding: 0 0.3333em;
-    border-radius: 2px;
-}
-code {
-    font-family: "Hack", monospace;
-    font-size: 9pt;
-}
-:not(pre) > code {
-    display: inline-block;
-    vertical-align: calc(5%);
-}
-img {
-    max-width: 100%;
-}
-a {
-    color: #2492ff;
+
+a { 
+    color: #0645ad; 
     text-decoration: underline;
 }
-a:hover {
+
+a:hover { 
+    color: #06e; 
     text-decoration: none;
 }
-ul {
-    list-style-type: circle;
+
+a:active {
+    color:#faa700;
 }
-ol {
-    list-style-type: decimal-leading-zero;
+
+p {
+    margin: 1rem 0;
 }
-ul,ol {
+
+img {
+    max-width: 100%;
+    vertical-align: middle;
+}
+
+h1, h2, h3, h4, h5, h6 {
+    font-weight: normal;
+    line-height: 1;
+    color: #111;
+}
+
+h4, h5, h6 {
+    font-weight: bold;
+}
+
+h1 {
+    font-size: 2.5rem;
+}
+
+h2 {
+    font-size: 2rem;
+}
+
+h3 {
+    font-size: 1.5rem;
+}
+
+h4 {
+    font-size: 1.2rem;
+}
+
+h5 {
+    font-size: 1rem;
+}
+
+h6 {
+    font-size: 0.9rem;
+}
+
+blockquote {
+    color: #666;
     margin: 0;
-    padding: 0;
-    list-style-position: outside;
+    padding-left: 3rem;
+    border-left: 0.5rem solid #eee;
 }
-ul ul, ul ol, ol ul, ol ol {
-    margin-left: 2rem;
+
+hr {
+    display: block;
+    height: 2px;
+    border: 0;
+    border-top: 1px solid #aaa;
+    border-bottom: 1px solid #eee;
+    margin: 1rem 0;
+    padding: 0;
+}
+
+pre, code, kbd, samp {
+    background: #444;
+    color: #fefefe;
+    font-family: "Hack", monospace;
+    font-size: 0.98rem;
+    border-radius: 2px;
+}
+
+pre {
+    white-space: pre-wrap;
+    word-wrap: break-word;
+}
+
+:not(pre) > code {
+    padding: 0 0.3333rem;
+}
+
+dfn {
+    font-style: italic;
+}
+
+ins {
+    background: #ff9;
+    color: #000;
+    text-decoration: none;
+}
+
+mark {
+    background: #ff0;
+    color: #000;
+    font-style: italic;
+    font-weight: bold;
+}
+
+sub, sup {
+    font-size: 75%;
+    line-height: 0;
+    position: relative;
+    vertical-align: baseline;
+}
+
+sup {
+    top: -0.5em;
+}
+
+sub {
+    bottom: -0.25em;
+}
+
+ul, ol {
+    margin: 1rem 0;
+    padding: 0 0 0 2em;
+}
+
+li p:last-child {
+    margin: 0;
+}
+
+dd {
+    margin: 0 0 0 2rem;
+}
+
+table {
+    border-radius: 5px;
+    border-collapse: collapse;
+    border-spacing: 0;
+    width: 100%;
+}
+
+th {
+    border-bottom: 1px solid black;
+}
+
+td {
+    vertical-align: top;
 }
 
 a:not([href]) {
@@ -188,9 +271,6 @@ a:not([href])::before {
     position: absolute;
     left: -1em;
     pointer-events: none;
-}
-table {
-    border-radius: 5px;
 }
 
 h1[id],h2[id],h3[id],h4[id],h5[id],h6[id] {
@@ -217,9 +297,6 @@ a.hashbang:hover {
     text-decoration: underline;
 }
 @media (max-width: 800px) {
-    ul,ol {
-        list-style-position: inside;
-    }
     h1[id],h2[id],h3[id],h4[id],h5[id],h6[id] {
         left: 3rem;
         width: calc(100% - 3rem);
